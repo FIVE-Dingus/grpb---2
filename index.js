1854497a99e9368828196b88093d564417946a25
TranquillouMAN <beney7189@gmail.com>
2022-12-05 14:15:15 +0100

    crée

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dbo = require("./db/db");
+const app = express();
+const port = 4444;
+
+dbo.connectToServer();
+
+app.listen(port, function () {
+    console.log(`App listening on port ${port}!`);
+  });
+
+app.get("/pokemon", function (req, res) {
+    //on se connecte à la DB MongoDB
+    const dbConnect = dbo.getDb();
+    //premier test permettant de récupérer mes pokemons !
+    dbConnect
+      .collection("pokemon")
+      .find({}) // permet de filtrer les résultats
+      /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).send("Error fetching pokemons!");
+        } else {
+          res.json(result);
+        }
+      });
+      /*
+      Bref lisez la doc, 
+      il y a plein de manières de faire ce qu'on veut :) 
+      */
+      
+  });
