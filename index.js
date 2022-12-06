6567cb1b860a4cea5fc442d7a3db87900beb1302
TranquillouMAN <beney7189@gmail.com>
2022-12-06 15:45:36 +0100

    964

--- index.js
+++ index.js
@@ -47,6 +47,22 @@ app.get("/pokemon", function (req, res) {
         }
       });      
   });
+  app.get("/types", function (req, res) {
+    //on se connecte à la DB MongoDB
+    const dbConnect = dbo.getDb();
+    //premier test permettant de récupérer mes pokemons !
+    dbConnect
+      .collection("types")
+      .find({}) // permet de filtrer les résultats
+      /*.limit(50) // pourrait permettre de limiter le nombre de résultats */
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).send("Error fetching pokemons!");
+        } else {
+          res.json(result);
+        }
+      });      
+  });
 app.post('/pokemon/insert', jsonParser, (req, res) => {
     const body = req.body;
     console.log('Got body:', body);
@@ -63,6 +79,16 @@ app.post('/pokedex/insert', jsonParser, (req, res) => {
   const dbConnect = dbo.getDb();
   dbConnect
     .collection("pokedex")
+    .insert({...body})
+  res.json(body);
+});
+
+app.post('/types/insert', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect
+    .collection("types")
     .insert({...body})
   res.json(body);
 });
@@ -99,14 +125,36 @@ app.delete('/pokedex', jsonParser, (req, res) => {
     });    
   res.status(200).json('Product deleted');
 })
+
+app.delete('/types', jsonParser, (req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect
+    .collection("types")
+    .deleteOne({...body})
+    .then(function (result, err) {
+      if (err) {
+        res.status(400).send(err.message);
+      } else {
+        res.json(result);
+      }
+    });    
+  res.status(200).json('Product deleted');
+})
+
 app.post('/pokemon/update', jsonParser,(req, res) => {
   const body = req.body;
   console.log('Got body:', body);
   const dbConnect = dbo.getDb();
   dbConnect.collection("pokemon").updateOne(
     { name: body.name },
-    {
-      $set: { newname: body.newname },
+    { 
+      $set: { 
+        name: body.newname,
+        type1:body.type,
+        type2:body.type2
+      },
     }
   ).then(function(result, err){
     if (err) {
@@ -123,7 +171,11 @@ app.post('/pokedex/update', jsonParser,(req, res) => {
   dbConnect.collection("pokedex").updateOne(
     { name: body.name },
     {
-      $set: { newname: body.newname },
+      $set: { 
+        newname: body.newname,
+        type1:body.type1,
+        type2:body.type2
+      },
     }
   ).then(function(result, err){
     if (err) {
@@ -133,3 +185,41 @@ app.post('/pokedex/update', jsonParser,(req, res) => {
     }
   })
 })
+
+app.post('/types/update', jsonParser,(req, res) => {
+  const body = req.body;
+  console.log('Got body:', body);
+  const dbConnect = dbo.getDb();
+  dbConnect.collection("types").updateOne(
+    { name: body.name },
+    {
+      $set: { 
+        newname: body.newname,
+       },
+    }
+  ).then(function(result, err){
+    if (err) {
+      res.status(400).send(err.message);
+    } else {
+      res.json(result);
+    }
+  })
+})
+
+// app.post('/pokemon/type', jsonParser,(req, res) => {
+//   const body = req.body;
+//   console.log('Got body:', body);
+//   const dbConnect = dbo.getDb();
+//   dbConnect.collection("pokemon").updateOne(
+//     { name: body.name },
+//     { 
+//       $set: { type: body. },
+//     }
+//   ).then(function(result, err){
+//     if (err) {
+//       res.status(400).send(err.message);
+//     } else {
+//       res.json(result);
+//     }
+//   })
+// })
